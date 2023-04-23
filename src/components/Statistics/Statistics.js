380e460c78e692f772231d1b80cad5f4ac6ccc1b
Sharuda <sharuda.yana98@gmail.com>
2023-04-23 16:38:20 +0300

    add Profile

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+const Statistics = ({ stats, title }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">Upload stats</h2>
+
+      <ul class="stat-list">
+        <li class="item">
+          <span class="label">.docx</span>
+          <span class="percentage">4%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp3</span>
+          <span class="percentage">14%</span>
+        </li>
+        <li class="item">
+          <span class="label">.pdf</span>
+          <span class="percentage">41%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp4</span>
+          <span class="percentage">12%</span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number).isRequired,
+};
+
+export default Statistics;
