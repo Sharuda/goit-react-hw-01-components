ac7dfeed7a7c80e5ce0ca663b842c42d65edb6e4
Sharuda <sharuda.yana98@gmail.com>
2023-04-25 21:09:17 +0300

    add transaction init files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends';
+import transactions from '../transactions';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
