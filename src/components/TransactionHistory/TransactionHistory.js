3cb7fe49040431fd1fa04b5c3f60db3311a6404d
Sharuda <sharuda.yana98@gmail.com>
2023-04-26 20:29:13 +0300

    add section transaction

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,3 +1,46 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import {
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tb,
+} from '../TransactionHistory/TransactionHistory.styled';
 
-export const TransactionHistory = ({ items }) => {};
+export const TransactionHistory = ({ items }) => {
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Type</Th>
+          <Th>Amount</Th>
+          <Th>Currency</Th>
+        </Tr>
+      </Thead>
+
+      <Tb>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <Tr key={id}>
+              <Td>{type}</Td>
+              <Td>{amount}</Td>
+              <Td>{currency}</Td>
+            </Tr>
+          );
+        })}
+      </Tb>
+    </Table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const Table = styled.table`
+  box-shadow: -0.0625rem -0.0625rem 0.0625rem #fff,
+    0.0625rem 0.0625rem 0.0625rem #babecc;
+`;
+
+export const Thead = styled.thead`
+  font-size: 1rem;
+  text-transform: uppercase;
+`;
+
+export const Tr = styled.tr`
+  background-color: lightblue;
+`;
+
+export const Th = styled.th`
+  width: 300px;
+  height: 35px;
+  font-size: 12px;
+  font-weight: 500;
+  color: white;
+  background-color: blue;
+`;
+
+export const Tb = styled.tbody``;
+
+export const Td = styled.td`
+  text-align: center;
+`;
